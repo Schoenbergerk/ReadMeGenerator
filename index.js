fbdf7f7a6bd4bb92e7d1a594d583f683c3f33948
Kyle <schoenbergerk@hotmail.com>
2024-04-20 16:43:42 -0500

    setting up project and added questions

---
+++ index.js
@@ -0,0 +1,60 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please describe your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the installation instructions.',
+        name: 'instructions',
+    },
+    {
+        type: 'input',
+        message: 'Please enter instructions or information on how to use the application.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the guidelines for contribution?',
+        name: 'contribution',
+    },
+    {
+        type: 'list',
+        message: 'Please select the applicable license for this project.',
+        name: 'license',
+        choices: ['MIT', 'Apache License 2.0', 'GNU General Public License (GPL)', 'BSD 3-Clause License', 'Mozilla Public License 2.0', 'None',]
+    },
+    {
+        type: 'input',
+        message: 'Please enter information on how to test the application',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message:'What is your email address?',
+        name: 'email',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
